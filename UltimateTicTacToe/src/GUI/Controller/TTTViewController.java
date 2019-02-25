5394a1bda9a06f9aa9915eff0b37a6b6eac3ff19
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-02-25 12:25:40 +0100

    fx:id på btns slettet

--- UltimateTicTacToe/src/GUI/Controller/TTTViewController.java
+++ UltimateTicTacToe/src/GUI/Controller/TTTViewController.java
@@ -24,185 +24,23 @@ public class TTTViewController implements Initializable
     @FXML
     private GridPane macroBoard;
     @FXML
-    private GridPane gridPane;
+    private GridPane board1;
     @FXML
-    private Button btn1;
+    private GridPane board2;
     @FXML
-    private Button btn2;
+    private GridPane board4;
     @FXML
-    private Button btn3;
+    private GridPane board5;
     @FXML
-    private Button btn4;
+    private GridPane board7;
     @FXML
-    private Button btn5;
+    private GridPane board8;
     @FXML
-    private Button btn6;
+    private GridPane board9;
     @FXML
-    private Button btn7;
+    private GridPane board6;
     @FXML
-    private Button btn8;
-    @FXML
-    private Button btn9;
-    @FXML
-    private GridPane gridPane1;
-    @FXML
-    private Button btn11;
-    @FXML
-    private Button btn21;
-    @FXML
-    private Button btn31;
-    @FXML
-    private Button btn41;
-    @FXML
-    private Button btn51;
-    @FXML
-    private Button btn61;
-    @FXML
-    private Button btn71;
-    @FXML
-    private Button btn81;
-    @FXML
-    private Button btn91;
-    @FXML
-    private GridPane gridPane2;
-    @FXML
-    private Button btn12;
-    @FXML
-    private Button btn22;
-    @FXML
-    private Button btn32;
-    @FXML
-    private Button btn42;
-    @FXML
-    private Button btn52;
-    @FXML
-    private Button btn62;
-    @FXML
-    private Button btn72;
-    @FXML
-    private Button btn82;
-    @FXML
-    private Button btn92;
-    @FXML
-    private GridPane gridPane3;
-    @FXML
-    private Button btn13;
-    @FXML
-    private Button btn23;
-    @FXML
-    private Button btn33;
-    @FXML
-    private Button btn43;
-    @FXML
-    private Button btn53;
-    @FXML
-    private Button btn63;
-    @FXML
-    private Button btn73;
-    @FXML
-    private Button btn83;
-    @FXML
-    private Button btn93;
-    @FXML
-    private GridPane gridPane4;
-    @FXML
-    private Button btn14;
-    @FXML
-    private Button btn24;
-    @FXML
-    private Button btn34;
-    @FXML
-    private Button btn44;
-    @FXML
-    private Button btn54;
-    @FXML
-    private Button btn64;
-    @FXML
-    private Button btn74;
-    @FXML
-    private Button btn84;
-    @FXML
-    private Button btn94;
-    @FXML
-    private GridPane gridPane5;
-    @FXML
-    private Button btn15;
-    @FXML
-    private Button btn25;
-    @FXML
-    private Button btn35;
-    @FXML
-    private Button btn45;
-    @FXML
-    private Button btn55;
-    @FXML
-    private Button btn65;
-    @FXML
-    private Button btn75;
-    @FXML
-    private Button btn85;
-    @FXML
-    private Button btn95;
-    @FXML
-    private GridPane gridPane6;
-    @FXML
-    private Button btn16;
-    @FXML
-    private Button btn26;
-    @FXML
-    private Button btn36;
-    @FXML
-    private Button btn46;
-    @FXML
-    private Button btn56;
-    @FXML
-    private Button btn66;
-    @FXML
-    private Button btn76;
-    @FXML
-    private Button btn86;
-    @FXML
-    private Button btn96;
-    @FXML
-    private GridPane gridPane7;
-    @FXML
-    private Button btn17;
-    @FXML
-    private Button btn27;
-    @FXML
-    private Button btn37;
-    @FXML
-    private Button btn47;
-    @FXML
-    private Button btn57;
-    @FXML
-    private Button btn67;
-    @FXML
-    private Button btn77;
-    @FXML
-    private Button btn87;
-    @FXML
-    private Button btn97;
-    @FXML
-    private GridPane gridPane8;
-    @FXML
-    private Button btn18;
-    @FXML
-    private Button btn28;
-    @FXML
-    private Button btn38;
-    @FXML
-    private Button btn48;
-    @FXML
-    private Button btn58;
-    @FXML
-    private Button btn68;
-    @FXML
-    private Button btn78;
-    @FXML
-    private Button btn88;
-    @FXML
-    private Button btn98;
+    private GridPane board3;
     
     @FXML
     private void handleButtonAction(ActionEvent event)
