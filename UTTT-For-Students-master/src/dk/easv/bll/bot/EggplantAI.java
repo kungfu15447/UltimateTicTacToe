90eb8365fef48c0af2883f14d2660dbdc85cda4f
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-03-05 22:03:46 +0100

    Prøvet ting og fejlet som sædvanligt

--- UTTT-For-Students-master/src/dk/easv/bll/bot/EggplantAI.java
+++ UTTT-For-Students-master/src/dk/easv/bll/bot/EggplantAI.java
@@ -8,147 +8,118 @@ package dk.easv.bll.bot;
 import dk.easv.bll.field.IField;
 import dk.easv.bll.game.IGameState;
 import dk.easv.bll.move.IMove;
-import dk.easv.bll.move.Move;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author Frederik Jensen
  */
-public class EggplantAI implements IBot
-{
+public class EggplantAI implements IBot {
+
     private final String BOT_NAME = "EggplantAI";
     private String player = "";
     private String enemy = "";
-    int[][] preferredMoves = {
-            {0, 0}, {2, 2}, {0, 2}, {2, 0}, //Corners
-            {1, 1}, //Middle
-            {0, 1}, {2, 1}, {1, 0}, {1, 2}}; //Outer Middles ordered across
-   
-    
-    @Override
-    public IMove doMove(IGameState state)
-    {
-        checkWhoIAm(state);
-        //Find macroboard to play in
-        for (int[] move : preferredMoves)
-        {
-            if (checkForWins(state) != null) {
-                    return checkForWins(state);
-                }
-            if(state.getField().getMacroboard()[move[0]][move[1]].equals(IField.AVAILABLE_FIELD))
-            {
-                
-                //find move to play
-                for (int[] selectedMove : preferredMoves)
-                {
-                    int x = move[0]*3 + selectedMove[0];
-                    int y = move[1]*3 + selectedMove[1];
-                    if(state.getField().getBoard()[x][y].equals(IField.EMPTY_FIELD))
-                    {
-                        return new Move(x,y);
-                    }
-                }
-            }
-        }
+    private Random rand = new Random();
 
-        //NOTE: Something failed, just take the first available move I guess!
+    @Override
+    public IMove doMove(IGameState state) {
+        List<IMove> moves = state.getField().getAvailableMoves();
+
+        checkWhoIAm(state);
+        if (checkForWins(state, player) != null) {
+            return checkForWins(state, player);
+        } else if (checkForWins(state, enemy) != null) {
+            return checkForWins(state, enemy);
+        }
+        if (moves.size() > 0) {
+            return moves.get(rand.nextInt(moves.size()));
+            /* get random move from available moves */
+        }
         return state.getField().getAvailableMoves().get(0);
     }
 
     @Override
-    public String getBotName()
-    {
+    public String getBotName() {
         return BOT_NAME;
     }
-    
-    private IMove checkForWins(IGameState state)
-    {
-        String[][] board = state.getField().getBoard();
+
+    private IMove checkForWins(IGameState state, String currentPlayer) {
         List<IMove> availableMoves = state.getField().getAvailableMoves();
-        for (IMove move : availableMoves)
-        {
-            board[move.getX()][move.getY()] = player;
+        for (IMove move : availableMoves) {
+            String[][] board = state.getField().getBoard();
+            board[move.getX()][move.getY()] = currentPlayer;
             int localX = move.getX() % 3;
             int localY = move.getY() % 3;
             int startX = move.getX() - (localX);
             int startY = move.getY() - (localY);
             //check col
-            for (int i = startY; i < startY + 3; i++)
-            {
-                if (!board[move.getX()][i].equals(player))
-                {
+            for (int i = startY; i < startY + 3; i++) {
+                if (!board[move.getX()][i].equals(currentPlayer)) {
                     break;
                 }
-                if (i == startY + 3 - 1)
-                {
+                if (i == startY + 3 - 1) {
                     return move;
                 }
             }
 
             //check row
-            for (int i = startX; i < startX + 3; i++)
-            {
-                if (!board[i][move.getY()].equals(player))
-                {
+            for (int i = startX; i < startX + 3; i++) {
+                if (!board[i][move.getY()].equals(currentPlayer)) {
                     break;
                 }
-                if (i == startX + 3 - 1)
-                {
+                if (i == startX + 3 - 1) {
                     return move;
                 }
             }
 
             //check diagonal
-            if (localX == localY)
-            {
+            if (localX == localY) {
                 //we're on a diagonal
                 int y = startY;
-                for (int i = startX; i < startX + 3; i++)
-                {
-                    if (!board[i][y++].equals(player))
-                    {
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][y++].equals(currentPlayer)) {
                         break;
                     }
-                    if (i == startX + 3 - 1)
-                    {
+                    if (i == startX + 3 - 1) {
                         return move;
                     }
                 }
             }
 
             //check anti diagonal
-            if (localX + localY == 3 - 1)
-            {
+            if (localX + localY == 3 - 1) {
                 int less = 0;
-                for (int i = startX; i < startX + 3; i++)
-                {
-                    if (!board[i][(startY + 2) - less++].equals(player))
-                    {
+                for (int i = startX; i < startX + 3; i++) {
+                    if (!board[i][(startY + 2) - less++].equals(currentPlayer)) {
                         break;
                     }
-                    if (i == startX + 3 - 1)
-                    {
+                    if (i == startX + 3 - 1) {
                         return move;
                     }
                 }
             }
+            board[move.getX()][move.getY()] = IField.EMPTY_FIELD;
         }
-        
-        return null;
+
+        return state.getField().getAvailableMoves().get(0);
     }
-    
+
     private void checkWhoIAm(IGameState state) {
         if (player.equals("") && enemy.equals("")) {
             if (state.getField().isEmpty()) {
                 player = "0";
                 enemy = "1";
-                
-            }else{
+
+            } else {
                 player = "1";
                 enemy = "0";
             }
         }
     }
     
+    private boolean checkIfEnemyWinsNextBoard(IGameState state, IMove move) {
+        return false;
+    }
+
 }
